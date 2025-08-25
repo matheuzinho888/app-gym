d1d146b4893f902063f1762342280d1653bce8a9
Matheus Prudente <matheusprutrevigno292@gmail.com>
2025-08-25 03:05:33 -0300

    new

--- projeto/website/urls.py
+++ projeto/website/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path ('',views.home, name='home'),
     path('proxpag/',views.contato,name='proxpag'),
-}
+    path('dieta/', views.dieta, name='dieta'),
+
+  #  path('pessoas/', views.lista_pessoas, name='lista_pessoas'),
+]
